47a9da4f989caa42d972328670c60d7c41f63d59
MTHermawan <trigusnihermawan@gmail.com>
2026-02-25 00:18:25 +0800

    Post-Test 1

---
+++ POSTTEST_VIS/POSTTEST_1/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnTambah = New Button()
+        txtIPS = New Label()
+        txtInputIPS = New TextBox()
+        txtIPK = New Label()
+        btnReset = New Button()
+        txtInputIPK = New TextBox()
+        txtKeterangan = New Label()
+        SuspendLayout()
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(344, 158)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 0
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' txtIPS
+        ' 
+        txtIPS.AutoSize = True
+        txtIPS.Location = New Point(219, 125)
+        txtIPS.Name = "txtIPS"
+        txtIPS.Size = New Size(86, 20)
+        txtIPS.TabIndex = 1
+        txtIPS.Text = "IP Semester"
+        ' 
+        ' txtInputIPS
+        ' 
+        txtInputIPS.Location = New Point(344, 125)
+        txtInputIPS.Name = "txtInputIPS"
+        txtInputIPS.Size = New Size(194, 27)
+        txtInputIPS.TabIndex = 2
+        ' 
+        ' txtIPK
+        ' 
+        txtIPK.AutoSize = True
+        txtIPK.Location = New Point(219, 224)
+        txtIPK.Name = "txtIPK"
+        txtIPK.Size = New Size(89, 20)
+        txtIPK.TabIndex = 3
+        txtIPK.Text = "IP Kumulatif"
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(444, 158)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(94, 29)
+        btnReset.TabIndex = 4
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' txtInputIPK
+        ' 
+        txtInputIPK.Location = New Point(344, 221)
+        txtInputIPK.Name = "txtInputIPK"
+        txtInputIPK.ReadOnly = True
+        txtInputIPK.Size = New Size(194, 27)
+        txtInputIPK.TabIndex = 5
+        ' 
+        ' txtKeterangan
+        ' 
+        txtKeterangan.AutoSize = True
+        txtKeterangan.Location = New Point(344, 267)
+        txtKeterangan.Name = "txtKeterangan"
+        txtKeterangan.Size = New Size(0, 20)
+        txtKeterangan.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtKeterangan)
+        Controls.Add(txtInputIPK)
+        Controls.Add(btnReset)
+        Controls.Add(txtIPK)
+        Controls.Add(txtInputIPS)
+        Controls.Add(txtIPS)
+        Controls.Add(btnTambah)
+        Name = "Form1"
+        Text = "Kalkulator IP"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents txtIPS As Label
+    Friend WithEvents txtInputIPS As TextBox
+    Friend WithEvents txtIPK As Label
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtInputIPK As TextBox
+    Friend WithEvents txtKeterangan As Label
+
+End Class
---
+++ POSTTEST_VIS/POSTTEST_1/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Dim jumlahIPS As Double = 0
+    Dim penjumlahanIPS As Double = 0
+    Dim nilaiIPK As Double = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim nilaiIPS As Double = 0
+        If (Not Double.TryParse(txtInputIPS.Text, nilaiIPS)) Then
+            MessageBox.Show("Input harus angka!")
+            Return
+        ElseIf (nilaiIPS < 0) Then
+            MessageBox.Show("Input tidak boleh kurang dari 0!")
+            Return
+        ElseIf (nilaiIPS > 4) Then
+            MessageBox.Show("Input tidak boleh lebih dari 4!")
+            Return
+        End If
+
+        jumlahIPS += 1
+        penjumlahanIPS += nilaiIPS
+        nilaiIPK = penjumlahanIPS / jumlahIPS
+
+        txtInputIPS.Text = ""
+        txtInputIPK.Text = nilaiIPK
+        txtKeterangan.Text = ""
+        If (nilaiIPK >= 2 And nilaiIPK <= 2.75) Then
+            txtKeterangan.Text = "Cukup"
+        ElseIf (nilaiIPK >= 2.76 And nilaiIPK <= 3) Then
+            txtKeterangan.Text = "Memuaskan"
+        ElseIf (nilaiIPK > 3.01) Then
+            txtKeterangan.Text = "Sangat Memuaskan"
+        Else
+            txtKeterangan.Text = "Well, well, well..."
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        jumlahIPS = 0
+        penjumlahanIPS = 0
+        nilaiIPK = 0
+
+        txtInputIPS.Text = ""
+        txtInputIPK.Text = ""
+        txtKeterangan.Text = ""
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
